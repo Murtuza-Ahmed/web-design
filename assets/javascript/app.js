95746b7b3b5a161e32fa55918615fed431d2ce28
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2023-09-28 00:16:15 +0500

    project not  compelet

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,10 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+};
+
 let sections = document.querySelectorAll("section");
 let navlinks = document.querySelectorAll("header nav a");
 window.onscroll = () => {
@@ -13,5 +20,6 @@ window.onscroll = () => {
             });
         };
     });
+    menuIcon.classList.remove("bx-x");
+    navbar.classList.remove("active");
 };
-
